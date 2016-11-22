406d47398b0a9ce579ffb855d35af482adf41461
Tan HP Laptop <trungtanbui@gmail.com>
2016-11-22 11:46:46 +0200

    ngIf, event binding, property binding

--- 2_tour_of_heros/app/app.component.ts
+++ 2_tour_of_heros/app/app.component.ts
@@ -25,10 +25,19 @@ const HEROES: Hero[] = [
   
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
+  
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+    </div>
+  </div>
   `,
   styles: [`
   .selected {
@@ -84,4 +93,9 @@ const HEROES: Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
