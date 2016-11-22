8d93b95d889c21952a77f34cce2bbb93b83a36ff
Tan HP Laptop <trungtanbui@gmail.com>
2016-11-22 10:52:09 +0200

    copy code from branch 1

--- 2_tour_of_heros/app/app.component.ts
+++ 2_tour_of_heros/app/app.component.ts
@@ -1,48 +1,6 @@
-import { Component }       from 'angular2/core';
-import { HeroService }     from './hero.service';
-import { HeroesComponent } from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
-
-@RouteConfig([
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  },
-  {
-    path: '/detail/:id_placeholder',
-    name: 'HeroDetail',
-    component: HeroDetailComponent
-  },
-])
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a [routerLink]="['Dashboard']">Dashboard</a>
-      <a [routerLink]="['Heroes']">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
-  styleUrls: ['app/app.component.css'],
-  directives: [ROUTER_DIRECTIVES],
-  providers: [
-    ROUTER_PROVIDERS,
-    HeroService
-  ]
+  template: '<h1>Hello Angular!</h1>'
 })
-
-export class AppComponent {
-  title = 'Tour of Heroes';
-}
+export class AppComponent { }
--- 2_tour_of_heros/app/main.ts
+++ 2_tour_of_heros/app/main.ts
@@ -1,4 +1,4 @@
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
-
-bootstrap(AppComponent);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
