bb1ee0e058dcd0e24adbf530561d05266f6802b6
Tan HP Laptop <trungtanbui@gmail.com>
2016-11-22 10:45:35 +0200

    delete redundant files

--- 1_angular2-quickstart/app/main.ts
+++
@@ -1,9 +0,0 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
-
-bootstrap(AppComponent);
-
-//We might wish to render the first page of our application on the server to improve launch performance or facilitate SEO.
-//These targets require a different kind of bootstrap function that we'd import from a different library.
-
-
