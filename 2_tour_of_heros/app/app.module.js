506e3cea511504e2858a23670277fa47fe28784d
Tan HP Laptop <trungtanbui@gmail.com>
2016-11-22 15:05:49 +0200

    router for detail/:id view. add routerLink

--- 2_tour_of_heros/app/app.module.js
+++ 2_tour_of_heros/app/app.module.js
@@ -37,7 +37,11 @@ var AppModule = (function () {
                     {
                         path: 'dashboard',
                         component: dashboard_component_1.DashboardComponent
-                    }
+                    },
+                    {
+                        path: 'detail/:id',
+                        component: hero_detail_component_1.HeroDetailComponent
+                    },
                 ])
             ],
             declarations: [
--- 2_tour_of_heros/app/app.module.ts
+++ 2_tour_of_heros/app/app.module.ts
@@ -26,7 +26,11 @@ import { HeroService } from './hero.service';
             {
                 path: 'dashboard',
                 component: DashboardComponent
-            }
+            },
+            {
+                path: 'detail/:id',
+                component: HeroDetailComponent
+            },
         ])
     ],
     declarations: [
