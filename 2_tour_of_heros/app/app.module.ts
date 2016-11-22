af38c6fc2bbef3bffa7c2530962e5bbc821d7042
Tan HP Laptop <trungtanbui@gmail.com>
2016-11-22 14:44:21 +0200

    dashboard component with external html & css files

--- 2_tour_of_heros/app/app.module.ts
+++ 2_tour_of_heros/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard.component';
 import {HeroesComponent}   from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
     imports:      [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            },
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            }
+        ])
     ],
     declarations: [
         AppComponent,
         HeroDetailComponent,
-        HeroesComponent
+        HeroesComponent,
+        DashboardComponent
     ],
     bootstrap:    [ AppComponent ]
 })
